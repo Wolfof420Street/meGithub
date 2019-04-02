61adf35eadc3336fcb6dbff62b5a8eb82726fbd3
wolf <billodida420@gmail.com>
2019-04-02 12:11:55 +0300

    editing the service to display users and repos

--- src/app/user-profile/search.service.ts
+++ src/app/user-profile/search.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Users} from '../users';
+import {Repos} from '../repos';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
+  users: Users
+  repos: Repos
 
-  constructor() { }
+  constructor(private http:HttpClient) {
+    this.users = new Users ("","","","",0,false,new Date(),0,0);
+    this.repos = new Repos("","","",new Date())
+   }
 }
