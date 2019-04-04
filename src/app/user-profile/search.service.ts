5ff4db09e57eccf3ab1921dba7e6e91e202dc3a9
wolf <billodida420@gmail.com>
2019-04-04 11:23:46 +0300

    creating the page structure

--- src/app/user-profile/search.service.ts
+++ src/app/user-profile/search.service.ts
@@ -2,17 +2,31 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {Users} from '../users';
 import {Repos} from '../repos';
-import { getDefaultService } from 'selenium-webdriver/edge';
+import { resolve } from 'url';
+import { reject } from 'q'
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class SearchService {
   users: Users
   repos: Repos
+  newRepo:any;
+  searchRepo:any;
 
   constructor(private http:HttpClient) {
     this.users = new Users ("","","","",0,false,new Date(),0,0);
     this.repos = new Repos("","","",new Date())
    }
 }
+
+    interface ApiResponse{
+      name:string;
+      login:string;
+      avatar_url:string;
+      html_url:string;
+      public_repos:number;
+      hireable:boolean;
+      created_at:Date;
+      followers:number;
+      following:number;      
+    }
+  
